1ebec737e403d74d023e1e6f2d50b47654f99e27
Le Van Linh <levanlinh@sint.co.jp>
2018-11-14 16:01:04 +0900

    create component header for module flag game

---
+++ src/modules/countries/_components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1 align="center">Wavin' Flags 
+      <b-badge>{{ score }}</b-badge>
+      <span style="font-size: 20px"> / {{ numberOfCountries }}</span>
+    </h1>
+</template>
+
+<script>
+export default {
+    name: 'flag-game-header',
+    props: {
+        score: {
+            type: Number,
+            default: 0
+        },
+        numberOfCountries: {
+            type: Number,
+            default: 0
+        }
+    }
+}
+</script>
+
--- src/modules/countries/index.vue
+++ src/modules/countries/index.vue
@@ -1,9 +1,6 @@
 <template>
-    <b-container class="hello">
-        <h1 align="center">Wavin' Flags 
-          <b-badge>{{ score }}</b-badge>
-          <span v-if="countryGetAllData" style="font-size: 20px"> / {{ countryGetAllData.length }}</span>
-        </h1>
+    <b-container class="hello" v-if="countryGetAllData">
+        <FlagGameHeader :score="score" :numberOfCountries="countryGetAllData.length"/>
         <b-container class="bv-example-row" style="background-color: #99cc00; padding: 1rem" v-if="puzzle != null">
             <b-row>
                 <b-col cols="8" align="center">
@@ -39,90 +36,95 @@
 </template>
 
 <script>
-    const STORE_KEY = 'countries'
-    import store from './_store'
-    import { mapGetters, mapActions, mapMutations } from 'vuex'
-    import Puzzle from './puzzle'
-    import types from './_store/mutation-types'
-    // import { mapFields } from './_store/mapFields'
-    import _ from 'lodash'
 
-    export default {
-        name: 'CountriesIndex',
-        components: {},
-        data () {
-            return {
-                puzzle: null,
-                temp: [],
-                answerDisabled: false,
-                earthIcon: require('@/assets/earth_walking_hc.gif'),
-                correctIcon: require('@/assets/correct.gif'),
-                incorrectIcon: require('@/assets/incorrect.gif'),
-                resultIcon: null
-            }
-        },
-        computed: {
-            ...mapGetters({
-                countryGetAllData: STORE_KEY + '/countryGetAllData',
-                score: STORE_KEY + '/score',
-                currentCountry: STORE_KEY + '/currentCountry'
-            })
-        },
-        beforeCreate () {
-            if (!(STORE_KEY in this.$store._modules.root._children)) {
-                this.$store.registerModule(STORE_KEY, store)
-            }
-        },
-        mounted () {
-            this.getCountries()
-            .then(data => {
-                console.log(data)
-                this.shuffleCountries()
-                this.puzzle = this.createPuzzle()
-            })
+const STORE_KEY = 'countries'
+import store from './_store'
+import { mapGetters, mapActions, mapMutations } from 'vuex'
+import Puzzle from './puzzle'
+import types from './_store/mutation-types'
+// import { mapFields } from './_store/mapFields'
+import _ from 'lodash'
 
+import FlagGameHeader from './_components/Header'
+
+export default {
+    name: 'CountriesIndex',
+    components: {
+        FlagGameHeader
+    },
+    data () {
+        return {
+            puzzle: null,
+            temp: [],
+            answerDisabled: false,
+            earthIcon: require('@/assets/earth_walking_hc.gif'),
+            correctIcon: require('@/assets/correct.gif'),
+            incorrectIcon: require('@/assets/incorrect.gif'),
+            resultIcon: null
+        }
+    },
+    computed: {
+        ...mapGetters({
+            countryGetAllData: STORE_KEY + '/countryGetAllData',
+            score: STORE_KEY + '/score',
+            currentCountry: STORE_KEY + '/currentCountry'
+        })
+    },
+    beforeCreate () {
+        if (!(STORE_KEY in this.$store._modules.root._children)) {
+            this.$store.registerModule(STORE_KEY, store)
+        }
+    },
+    mounted () {
+        this.getCountries()
+        .then(data => {
+            console.log(data)
+            this.shuffleCountries()
+            this.puzzle = this.createPuzzle()
+        })
+
+        this.resultIcon = this.earthIcon
+    },
+    methods: {
+        ...mapActions({
+            getCountries: STORE_KEY + '/getCountries'
+        }),
+        ...mapMutations({
+            increaseCurrentCountry: STORE_KEY + '/' + types.INCREASE_CURRENT_COUNTRY,
+            increaseScore: STORE_KEY + '/' + types.INCREASE_SCORE,
+            shuffleCountries: STORE_KEY + '/' + types.SHUFFLE_COUNTRIES
+        }),
+        createPuzzle () {
+            this.increaseCurrentCountry()
+            this.temp = [...Array(this.countryGetAllData.length).keys()]
+            let currentIndex = this.currentCountry
+            let answerCountry = this.countryGetAllData[currentIndex]
+            this.temp.splice(currentIndex, 1)
+            let fakeCountries = []
+            for (let i = 0; i < 3; i++) {
+                let ranIndex = _.random(0, this.temp.length)
+                fakeCountries.push(this.countryGetAllData[ranIndex])
+                this.temp.splice(ranIndex, 1)
+            }
+            return new Puzzle(answerCountry, fakeCountries)
+        },
+        checkAnswer (country) {
+            this.answerDisabled = true
+            let isCorrect = this.puzzle.checkAnswer(country)
+            if (isCorrect) {
+            this.increaseScore()
+            this.resultIcon = this.correctIcon
+            return
+            }
+            this.resultIcon = this.incorrectIcon
+        },
+        nextPuzzle () {
             this.resultIcon = this.earthIcon
-        },
-        methods: {
-            ...mapActions({
-                getCountries: STORE_KEY + '/getCountries'
-            }),
-            ...mapMutations({
-                increaseCurrentCountry: STORE_KEY + '/' + types.INCREASE_CURRENT_COUNTRY,
-                increaseScore: STORE_KEY + '/' + types.INCREASE_SCORE,
-                shuffleCountries: STORE_KEY + '/' + types.SHUFFLE_COUNTRIES
-            }),
-            createPuzzle () {
-                this.increaseCurrentCountry()
-                this.temp = [...Array(this.countryGetAllData.length).keys()]
-                let currentIndex = this.currentCountry
-                let answerCountry = this.countryGetAllData[currentIndex]
-                this.temp.splice(currentIndex, 1)
-                let fakeCountries = []
-                for (let i = 0; i < 3; i++) {
-                    let ranIndex = _.random(0, this.temp.length)
-                    fakeCountries.push(this.countryGetAllData[ranIndex])
-                    this.temp.splice(ranIndex, 1)
-                }
-                return new Puzzle(answerCountry, fakeCountries)
-            },
-            checkAnswer (country) {
-              this.answerDisabled = true
-              let isCorrect = this.puzzle.checkAnswer(country)
-              if (isCorrect) {
-                this.increaseScore()
-                this.resultIcon = this.correctIcon
-                return
-              }
-              this.resultIcon = this.incorrectIcon
-            },
-            nextPuzzle () {
-              this.resultIcon = this.earthIcon
-              this.puzzle = this.createPuzzle()
-              this.answerDisabled = false
-            }
+            this.puzzle = this.createPuzzle()
+            this.answerDisabled = false
         }
     }
+}
 </script>
 
 <style>
